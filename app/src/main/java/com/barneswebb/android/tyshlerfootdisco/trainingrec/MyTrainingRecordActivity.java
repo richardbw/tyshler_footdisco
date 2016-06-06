ba129577b3d0d7586ac28ff51eced9a68b7d312c
rbw@sla-mobile.co.za <richard@barnes-webb.com>
2016-06-06 08:09:29 +0200

    Use a real JS construct, WTF knows why this works in chromium.

---
+++ app/src/main/java/com/barneswebb/android/tyshlerfootdisco/trainingrec/MyTrainingRecordActivity.java
@@ -0,0 +1,51 @@
+package com.barneswebb.android.tyshlerfootdisco.trainingrec;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.barneswebb.android.tyshlerfootdisco.R;
+
+public class MyTrainingRecordActivity extends AppCompatActivity implements TrainingFragment.OnListFragmentInteractionListener  {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mytrainingrecord);
+
+        MyTrainingRecordActivity.this.setTitle("My Training Record");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MyTrainingRecordActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Delete Record")
+                        .setMessage("Please confirm that you want to delete all exercise records?\n" +
+                                "(Cannot be undone)")
+                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override public void onClick(DialogInterface dialog, int which)  {
+                                ExerciseDataOpenHelper db = new ExerciseDataOpenHelper(MyTrainingRecordActivity.this);
+                                db.delAllData();
+                                MyTrainingRecordActivity.this.finish();
+                            } })
+                        .setNegativeButton("Cancel", null)
+                        .show();
+
+            }
+                    //db.delAllSMSes();
+
+        });
+    }
+
+    @Override
+    public void onListFragmentInteraction(ExerciseSession item) {
+        Log.d("ttsTraininRecAct", "wot goes 'ere?");
+        //wot goes 'ere?
+    }
+}
